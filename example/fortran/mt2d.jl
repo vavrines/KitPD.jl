26078b3a91c3d8c241b0e131ac85405e22d911a9
vavrines <tianbaixiao@gmail.com>
2023-08-22 13:13:30 +0800

    Finish julia draft

--- example/fortran/mt2d.jl
+++ example/fortran/mt2d.jl
@@ -3,9 +3,10 @@
 
 Reminder:
 - hs: heat source
+- ta & ma: Dimensionless coefficients in Eq. (13.65)
 
 Question:
-- ta & ma: Dimensionless coefficients in Eq. (13.65)
+
 """
 
 using KitBase.ProgressMeter: @showprogress
@@ -201,4 +202,120 @@ for i = 1:totnode
     tem[i, 1] = 0.0
     disp[i, 1] = 0.0
     disp[i, 2] = 0.0
+end
+
+@showprogress for tt = 1:5#nt
+    #--- temperature ---#
+    for i = 1:np
+        tem[i, 1] = 5.0 * dt * tt * exp(-2.0 * dt * tt)
+    end
+
+    for i = 1:totnode
+        pflux[i, 1] = 0.0
+        for j = 1:numfam[i, 1]
+            cnode = nodefam[pointfam[i, 1]+j-1, 1]
+            idist =
+                sqrt((coord[cnode, 1] - coord[i, 1])^2 + (coord[cnode, 2] - coord[i, 2])^2)
+            vartem = tem[cnode, 1] - tem[i, 1]
+            is = sqrt(
+                (coord[cnode, 1] + disp[cnode, 1] - coord[i, 1] - disp[i, 1])^2 +
+                (coord[cnode, 2] + disp[cnode, 2] - coord[i, 2] - disp[i, 2])^2,
+            )
+            ev =
+                (coord[cnode, 1] + disp[cnode, 1] - coord[i, 1] - disp[i, 1]) / is *
+                (vel[cnode, 1] - vel[i, 1]) +
+                (coord[cnode, 2] + disp[cnode, 2] - coord[i, 2] - disp[i, 2]) / is *
+                (vel[cnode, 2] - vel[i, 2])
+
+            if idist <= delta - radij
+                fac = 1.0
+            elseif idist <= delta + radij
+                fac = (delta + radij - idist) / (2.0 * radij)
+            else
+                fac = 0.0
+            end
+
+            scrt = (fncsth[i, 1] + fncsth[cnode, 1]) / 2.0
+            dflux1 = ta * (vartem / idist - 0.5 * cc * ev) * vol * scrt * fac
+
+            pflux[i, 1] += dflux1
+        end
+    end
+
+    for i = 1:totnode
+        tem[i, 1] += dt * (pflux[i, 1] + hs[i, 1])
+    end
+
+    #--- mechanics ---#
+    for i = (mp+1):totnode
+        disp[i, 1] = 0.0
+        disp[i, 2] = 0.0
+        vel[i, 1] = 0.0
+        vel[i, 2] = 0.0
+    end
+
+    for i = 1:np
+        bforce[i, 1] = 5.0 * dt * tt * exp(-2.0 * dt * tt) / (dx * nbnd)
+        bforce[i, 2] = 0.0
+    end
+
+    for i = 1:totnode
+        pforce[i, 1] = 0.0
+        pforce[i, 2] = 0.0
+        for j = 1:numfam[i, 1]
+            cnode = nodefam[pointfam[i, 1]+j-1, 1]
+            idist =
+                sqrt((coord[cnode, 1] - coord[i, 1])^2 + (coord[cnode, 2] - coord[i, 2])^2)
+            is = sqrt(
+                (coord[cnode, 1] + disp[cnode, 1] - coord[i, 1] - disp[i, 1])^2 +
+                (coord[cnode, 2] + disp[cnode, 2] - coord[i, 2] - disp[i, 2])^2,
+            )
+
+            if idist <= delta - radij
+                fac = 1.0
+            elseif idist <= delta + radij
+                fac = (delta + radij - idist) / (2.0 * radij)
+            else
+                fac = 0.0
+            end
+
+            if (abs(coord[cnode, 2] - coord[i, 2]) <= 1e-10)
+                theta = 0.0
+            elseif (abs(coord[cnode, 1] - coord[i, 1]) <= 1e-10)
+                theta = 90.0 * pi / 180.0
+            else
+                theta = atan(
+                    abs(coord[cnode, 2] - coord[i, 2]) / abs(coord[cnode, 1] - coord[i, 1]),
+                )
+            end
+
+            scx = (fncstm[i, 1] + fncstm[cnode, 1]) / 2.0
+            scy = (fncstm[i, 2] + fncstm[cnode, 2]) / 2.0
+            scr = 1.0 / (((cos(theta))^2.0 / (scx)^2) + ((sin(theta))^2.0 / (scy)^2))
+            scrm = sqrt(scr)
+
+            dforce1 =
+                ma * (coord[cnode, 1] + disp[cnode, 1] - coord[i, 1] - disp[i, 1]) / is *
+                vol *
+                scrm *
+                fac *
+                ((is - idist) / idist * 4.0 / 3.0 - (tem[cnode, 1] + tem[i, 1]) / 2)
+            pforce[i, 1] += dforce1
+
+            dforce2 =
+                ma * (coord[cnode, 2] + disp[cnode, 2] - coord[i, 2] - disp[i, 2]) / is *
+                vol *
+                scrm *
+                fac *
+                ((is - idist) / idist * 4.0 / 3.0 - (tem[cnode, 1] + tem[i, 1]) / 2)
+            pforce[i, 2] += dforce2
+        end
+    end
+
+    for i = 1:totnode
+        vel[i, 1] += dt * (pforce[i, 1] + bforce[i, 1])
+        disp[i, 1] += dt * vel[i, 1]
+        vel[i, 2] += dt * (pforce[i, 2] + bforce[i, 2])
+        disp[i, 2] += dt * vel[i, 2]
+    end
 end
